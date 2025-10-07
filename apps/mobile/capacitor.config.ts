1851d08d0806de1e8703d1476e8454e8e7ae1213
Anthony Bernardo <bernara3@gmail.com>
2025-10-07 09:11:15 +0200

    Initial commit

---
+++ apps/mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'foo-bar',
+  webDir: 'dist'
+};
+
+export default config;
